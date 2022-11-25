6305e3bfc34d542692f192248622b5dceadb500c
ajay-win10 <ajkumar173@gmail.com>
2022-11-26 01:33:26 +0530

    created banner section but content overflow from header-part

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
+
+var slideIndex = 0;
+showSlides();
 
 
-buttons.forEach(button =>{
-    button.addEventListener("click",()=>{
-        const offset = button.CDATA_SECTION_NODE.carouselButton === "next" ? 1 : -1
-        const slides = button
-            .closest("[data-carousel]")
-            .querySelector("[data-slides]")
+function showSlides(){
+    var i ;
+    var slides = document.getElementsByClassName("imageContainer");
+    var dots = document.getElementsByClassName("dot");
+// console.log(slides.length);
+    for (i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+    slideIndex++;
+    if(slideIndex>slides.length)
+    {
+        slideIndex=1;
 
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if(newIndex<0) newIndex = slides.children.length -1
-        if(newIndex >= slides.children.length ) newIndex = 0
-        slides.children[newIndex].dataset.active= true
-        delete activeSlide.dataset.active
-    })
-})
+    }
+    for(i =0;i<dots.length;i++)
+    {
+        dots.className = dots[i].className.replace(" active", "");
+
+    }
+    slides[slideIndex - 1 ].style.display="block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
