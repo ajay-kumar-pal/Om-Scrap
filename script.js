0a06c73f3efbb20e5f000ee8f8969f78a6c69ca8
ajay-win10 <ajkumar173@gmail.com>
2022-11-29 04:40:31 +0530

    rate list front end work complete

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let slideIndex = 0;
 showSlides();
-
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("imageContainer");
@@ -12,7 +11,3 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
-
-
-
