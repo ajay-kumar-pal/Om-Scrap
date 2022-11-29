62d7e01a6476814c5ff3c1fd8752a15ef49a3b20
ajay-win10 <ajkumar173@gmail.com>
2022-11-29 18:24:21 +0530

    working on admin panel

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+
+
+
+// function changebodercolor() {
+//   var chng = document.getElementById("userText").style.borderColor="red";
+//   // chng.style.borderColor = "rgb(190, 241, 255)";
+//   // chng.style.borderColor = "red";
+// // }
